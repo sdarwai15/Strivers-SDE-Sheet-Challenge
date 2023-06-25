337a6eff67da33ba905f57a946206c8426103124
Sourabh Darwai <sdarwai15@gmail.com>
2023-06-25 10:27:44 +0530

    Create 044. Minimum-Number-of-Platforms.cpp

---
+++ 044. Minimum-Number-of-Platforms.cpp
@@ -0,0 +1,22 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    sort(at,at+n);
+    sort(dt,dt+n);
+ 
+    int ans=1;
+    int count=1;
+    int i=1,j=0;
+
+    while(i<n && j<n){
+        if(at[i]<=dt[j]){
+            count++;
+            i++;
+        } else {
+            count--;
+            j++;
+        }
+
+        ans = max(ans,count);
+    }
+    
+    return ans;
+}
